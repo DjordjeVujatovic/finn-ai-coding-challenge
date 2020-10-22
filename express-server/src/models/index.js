0175449d1648c44ccfd5fbca6bc75d323caf962c
Djordje <djordje_v@hotmail.com>
2020-10-22 11:46:40 -0700

    Initialize sequelize and mock db's

---
+++ express-server/src/models/index.js
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import { Sequelize, DataTypes } from "sequelize";
+import user from "./user";
+
+// Initialize connection to postgres DB
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  null,
+  {
+    dialect: "postgres",
+  }
+);
+
+// DB models
+const models = {
+  User: user(sequelize, DataTypes),
+};
+
+// Check for connection to DB
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+export { sequelize };
+
+export default models;
